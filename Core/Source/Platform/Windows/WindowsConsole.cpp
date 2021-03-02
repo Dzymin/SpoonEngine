ec1853cc28b39be657f1b24bc7c0a1250a9d8c92
Dzymin <szymonsobczak5@gmail.com>
2021-03-02 15:59:41 +0100

    Removing useless code from WindowsConsole.cpp

--- Core/Source/Platform/Windows/WindowsConsole.cpp
+++ Core/Source/Platform/Windows/WindowsConsole.cpp
@@ -6,28 +6,10 @@ namespace Spoon
 	WindowsConsole::WindowsConsole()
 	{
 		AllocConsole();
-		FILE* file;
-		freopen_s(&file, "CONINT$", "r", stdin);
-		freopen_s(&file, "CONOUT$", "w", stdout);
-		freopen_s(&file, "CONOUT$", "w", stderr);
-
-		std::ios::sync_with_stdio(true);
-
-		std::wcout.clear();
-		std::cout.clear();
-		std::wcin.clear();
-		std::cin.clear();
-		std::wcerr.clear();
-		std::cerr.clear();
 	}
 	
 	WindowsConsole::~WindowsConsole()
 	{
-		FILE* file;
-		freopen_s(&file, "NUL:", "r", stdin);
-		freopen_s(&file, "NUL:", "w", stdout);
-		freopen_s(&file, "NUL:", "w", stderr);
-
 		FreeConsole();
 	}
 }
